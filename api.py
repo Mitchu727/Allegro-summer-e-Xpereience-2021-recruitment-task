8583091dc3a1c4187fbea5d8963caf603aa24bf3
Michu M <michal.mmtk@gmail.com>
2021-04-23 01:21:07 +0200

    Adding counting stars (the function not the song) #1

--- api.py
+++ api.py
@@ -1,24 +1,30 @@
 import requests
 
 
-class GithubUserInfo():
-    def __init__(self, name):
-        self.name = name
-        self.refresh()
+class GithubUserReposInfo():
+    def __init__(self, login):
+        self._login = login
+        self.load_repos()
 
-    def refresh(self):
-        self.response = requests.get("https://api.github.com/users/" + self.name)
-        self.repos = requests.get(self.response.json()['repos_url'])
+    def load_repos(self):
+        user_response = requests.get("https://api.github.com/users/" + self._login)
+        self._repos_url = user_response.json()['repos_url']
+        self._repos = requests.get(self._repos_url)
 
     def get_repos(self):
-        return self.repos
+        return self._repos
+
+    def get_star_number(self):
+        star_number = 0
+        for repo in self._repos.json():
+            star_number += repo['stargazers_count']
+        return star_number
 
     def list_repos(self):
-        for repo in self.repos.json():
+        for repo in self._repos.json():
             print(repo['name'])
 
 
-TestUser = GithubUserInfo("Mitchu727")
+TestUser = GithubUserReposInfo("Mitchu727")
 TestUser.list_repos()
-# print(TestUser.get_repos().json()[0]['name'])
-# print(TestUser.response.json()['login'])
+print(TestUser.get_star_number())
