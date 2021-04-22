3648e264b50da2af2b717394af1ea77a5f06a075
Michu M <michal.mmtk@gmail.com>
2021-04-23 00:49:02 +0200

    Adding listing repos #1

--- api.py
+++ api.py
@@ -8,8 +8,17 @@ class GithubUserInfo():
 
     def refresh(self):
         self.response = requests.get("https://api.github.com/users/" + self.name)
+        self.repos = requests.get(self.response.json()['repos_url'])
+
+    def get_repos(self):
+        return self.repos
+
+    def list_repos(self):
+        for repo in self.repos.json():
+            print(repo['name'])
 
 
 TestUser = GithubUserInfo("Mitchu727")
-print(TestUser.response.json())
-print(TestUser.response.json()['login'])
+TestUser.list_repos()
+# print(TestUser.get_repos().json()[0]['name'])
+# print(TestUser.response.json()['login'])
