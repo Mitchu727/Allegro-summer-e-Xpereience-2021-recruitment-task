254bb9725adeaccc51596be465cc5aa488b465be
Michu M <michal.mmtk@gmail.com>
2021-04-25 17:48:54 +0200

    Handling multiple pages of repositories

--- GithubUserReposInfo.py
+++ GithubUserReposInfo.py
@@ -1,5 +1,5 @@
 import requests
-import json
+
 
 class GithubUserReposInfo():
     def __init__(self, login):
@@ -7,21 +7,33 @@ class GithubUserReposInfo():
         self.load_repos()
 
     def load_repos(self):
-        user_response = requests.get("https://api.github.com/users/" + self._login)
-        self._repos_url = user_response.json()['repos_url']
-        self._repos = requests.get(self._repos_url)
+        self._repos = []
+        request = requests.get("https://api.github.com/users/" +
+                               self._login + "/repos?per_page=100&page=1")
+        page_numerator = 1
+        self.check_request(request)
+        while (len(request.json()) > 0):
+            for repo in request.json():
+                self._repos.append(repo)
+            page_numerator += 1
+            request = requests.get("https://api.github.com/users/" + self._login +
+                                   "/repos?per_page=100&page=" + str(page_numerator))
 
     def get_repos(self):
         return self._repos
 
-    def get_star_number(self):
+    def get_stars_number(self):
         star_number = 0
-        for repo in self._repos.json():
+        for repo in self._repos:
             star_number += repo['stargazers_count']
         return star_number
 
     def list_repos(self):
         repos_list = []
-        for repo in self._repos.json():
+        for repo in self._repos:
             repos_list.append({repo['name']: repo['stargazers_count']})
         return repos_list
+
+    def check_request(self, request):
+        if (request.status_code >= 400):
+            raise Exception(request.json()['message'])
